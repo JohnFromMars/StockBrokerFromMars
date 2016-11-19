c0e145c989f3b85ea5fb721574e8be1b1ecbb5f0
JohnFormMars <Yj@Yj-PC>
2016-11-19 16:46:29 +0800

    #2016/11/19 [BATCH]
    JobGetAllStcksId finished

--- BrokerBatchApp/src/com/stockbrokerfrommars/batch/job/JobGetAllStockId.java
+++ BrokerBatchApp/src/com/stockbrokerfrommars/batch/job/JobGetAllStockId.java
@@ -1,10 +1,47 @@
-package com.stockbrokerfrommars.batch.app;
+package com.stockbrokerfrommars.batch.job;
 
+import java.util.ResourceBundle;
+
+import com.batchfrommars.file.FileInformation;
+import com.batchfrommars.file.PhysicalFile;
+import com.stockbrokerfrommars.batch.component.getallstocksid.JobGetAllStocksIdGetData;
+
+/**
+ * 
+ * @author Yj
+ *
+ */
 public class JobGetAllStockId {
+	private static ResourceBundle resource;
+	private static FileInformation input;
+	private static FileInformation output;
+	private static JobGetAllStocksIdGetData getDataComponent;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		setResource();
+		setInput();
+		setOutput();
+		setGetDataComponent();
+		getDataComponent.start();
 
+	}
+
+	private static void setResource() {
+		resource = ResourceBundle.getBundle("com.stockbrokerfrommars.batch.config.getAllStocksId");
+	}
+
+	private static void setInput() {
+		input = new PhysicalFile(PhysicalFile.INPUT, resource.getString("dev.getAllStocksId.urlPath"), "UTF8", false);
+	}
+
+	private static void setOutput() {
+		output = new PhysicalFile(PhysicalFile.OUTPUT, resource.getString("dev.getAllStocksId.stocksIdData") + "stockId.txt", "UTF8", false);
+	}
+
+	private static void setGetDataComponent() {
+		getDataComponent = new JobGetAllStocksIdGetData();
+		getDataComponent.addInputFileInformation(input);
+		getDataComponent.addOutputFileInformation(output);
 	}
 
 }
